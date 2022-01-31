208a660500ed4a5b5004423244be95fadae2049f
rherik <84604794+rherik@users.noreply.github.com>
2022-01-30 23:15:16 -0300

    Update exercício-de-repeticao.py

--- exercício-de-repeticao.py
+++ exercício-de-repeticao.py
@@ -1,20 +1,3 @@
-"""
--> É uma lista de listas de números inteiros
--> As listas internas tem o tamanho de 10 elementos
--> As listas internas contém números entre 1 a 10 e eles podem ser duplicados
-Exercício
--> Crie uma função que encontra o primeiro duplicado considerando o segundo
-    número como a duplicação. Retorne a duplicação considerada.
-        Requisitos:
-            A ordem do número duplicado é considerada a partir da segunda
-            ocorrência do número, ou seja, o número duplicado em si.
-            Exemplo:
-                [1, 2, 3, ->3<-, 2, 1] -> 1, 2 e 3 são duplicados (retorne 3)
-                [1, 2, 3, 4, 5, 6] -> Retorne -1 (não tem duplicados)
-            Se não encontrar duplicados na lista, retorne -1
-"""
-
-
 def dupli(l):
     lista2 = set()
     for n in l:
